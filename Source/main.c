4e0c8f44177235c43ecd76dc14495c67a7af457a
BKaperick <bryankaperick@gmail.com>
2016-11-21 23:26:07 -0500

    Finished restructuring of files.  Ready to get back to work!

--- Source/main.c
+++ Source/main.c
@@ -5,19 +5,8 @@
 #include <stdio.h>
 #include "matrix.h"
 #include "frontend.h"
-#include "tests.h"
 
 int main() {
-	//float t = 100*(testAllAlgebra());	
-	//printf("%.2f percent of tests passed\n", t);
-
-	//TESTING
-	//parse("define A 2 2\n");
-	//parse("define B 2 2\n");
-	//parse("define C 2 2\n");
-	//parse("define D 2 2\n");
-	//parse("A+B+C\n");
-
 	char input[MAX_INPUT_SIZE];// = malloc(INPUT_SIZE * sizeof(char));
 	while (strcmp("exit\n", input) != 0) {
 		printf("> ");
